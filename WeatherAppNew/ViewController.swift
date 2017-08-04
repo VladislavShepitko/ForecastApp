cdb3815be5e1c9a9360c7951c3258214c3b35601
Vladyslav Shepitko <vladislavshepitko534@gmail.com>
2017-08-04 13:41:33 +0300

    Initial Commit

---
+++ WeatherAppNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAppNew
+//
+//  Created by Vladyslav Shepitko on 8/4/17.
+//  Copyright © 2017 Vladyslav Shepitko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
